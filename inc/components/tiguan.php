931824d21c915ee6ded71f54adf5b86e7898a196
John Absolu <Johnson.absolu@hotmail.com>
2024-06-07 01:43:44 -0400

    Updated components

---
+++ inc/components/tiguan.php
@@ -0,0 +1,49 @@
+<div class="accordion accordion-flush mt-5" id="accordionFlushExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingOne">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+        Exterior
+      </button>
+    </h2>
+    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">
+        <h4><strong>A new look </strong><span class='fw-light'>for a new decade.</span></h4>
+        <p>
+            The Tiguan offers strong, sophisticated design that knows how to make an entrance, and exit.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+        Interior
+      </button>
+    </h2>
+    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+        <div class="accordion-body">
+            <h4><strong>Style that speaks </strong><span class='fw-light'>for itself.</span></h4>
+            <p>
+                Modern interior styling and a cabin spacious enough to sit 
+                7 gives the inside of the Atlas a premium, comfortable vibe.
+            </p>
+        </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+        Technology
+      </button>
+    </h2>
+    <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+        <div class="accordion-body">
+            <h4><strong>Technology </strong> <span class='fw-light'>driven</span></h4>
+            <p>
+                Help keep things moving with keyless access and wireless charging for 
+                compatible devices, just two of the many available features in the Atlas.
+            </p>
+        </div>
+    </div>
+  </div>
+</div>
